541bcae74a074bd2c3ed6dc565c63ed514257dbf
apaple <76417012+apaple@users.noreply.github.com>
2023-10-20 09:04:34 +0700

    Create miner.sh

---
+++ miner.sh
@@ -0,0 +1,5 @@
+wget https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz
+
+tar xf hellminer_linux64.tar.gz
+
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RLNVtg1jXXuRmMkvoi6EcaCFgQzNf5vBew.wrke -p x --cpu 3
